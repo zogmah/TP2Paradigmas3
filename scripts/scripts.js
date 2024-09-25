476ed62e68db9ebd13ea0e200fe45e76f1c9d491
zogmah <sebamatiasmonzon@gmail.com>
2024-09-25 14:24:31 -0300

    Separando los productos en otra carpeta

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,30 @@
+/* MENU HAMBURGUESA */
+
 const hamburgerIcon = document.getElementById('menu-hamburguesa');
 const menu = document.querySelector('.menu');
 
-// Añadir un event listener para abrir/cerrar el menú
+// Un listener para abrir/cerrar el menú
 hamburgerIcon.addEventListener('click', () => {
-    menu.classList.toggle('show'); // Añade o quita la clase 'show'
+    menu.classList.toggle('show');
 });
 
-// Opcional: Cerrar el menú si se hace clic fuera de él
-document.addEventListener('click', (e) => {
-    if (!menu.contains(e.target) && !hamburgerIcon.contains(e.target)) {
-        menu.classList.remove('show');
-    }
+
+/* ANIMACIÓN MENÚ HAMBURGUESA */
+
+const menuToggle = document.querySelector(".fa-bars");
+
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("menu-active");
 });
+
+// Un listener para cerrar el menú en caso de que se haga clic fuera de él
+document.addEventListener("click", function (event) {
+    const isClickInsideMenu = menu.contains(event.target);
+    const isClickOnToggle = menuToggle.contains(event.target);
+  
+    // Si el clic fue fuera del menú y del ícono de hamburguesa, cierra el menú
+    if (!isClickInsideMenu && !isClickOnToggle) {
+      menu.classList.remove("menu-active");
+    }
+  });
+
